99b61f383470320e85fdc285f068d92f8d36c5fa
disha <dishavig1@gmail.com>
2022-02-25 08:13:35 +0530

    third project

---
+++ index.js
@@ -0,0 +1,79 @@
+//we want to trigger this function when a button is clicked
+//the function used is called anonymous function
+
+//DETECTING BUTTON PRESS:
+
+
+var numofButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numofButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+
+        //for all the collected buttons using for loop
+        // we perform the following events
+
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
+    });
+}
+
+
+//DETECTING KEYBOARD PRESS
+document.addEventListener("keydown", function (event) {
+    
+    makeSound(event.key);
+buttonAnimation(event.key);
+
+});
+
+
+//function for determining what sound to play when the respective key is pressed/clicked
+function makeSound(key) {
+    switch (key) {
+        case "w":
+
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play()
+            break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            console.log();
+    }
+}
+
+
+//to flash the buttons when pressed
+function buttonAnimation(currentkey) {
+var activeButton = document.querySelector("." +currentkey);
+activeButton.classList.add("pressed");
+
+setTimeout(function (){
+activeButton.classList.remove("pressed");
+}, 100)
+
+}
